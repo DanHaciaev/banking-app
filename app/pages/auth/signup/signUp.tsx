5be8f5684d6e4de62751ee38b293ace1413b6579
Danila Haciaev <danea48@gmail.com>
2025-04-17 15:47:10 +0300

    adding some edits on pages login, signup

--- app/pages/auth/signup/signUp.tsx
+++ app/pages/auth/signup/signUp.tsx
@@ -5,8 +5,7 @@ import Footer from "./footer/footer"
 
 export function SignUp() {
   return (
-    <main className="bg-[#1A1A1A] pt-[30px] h-fit relative z-[9999]">
-      <img src="/left-abstract.png" alt="" className="absolute top-0 w-[540px] z-[1]"/>
+    <main className="bg-[#1A1A1A] pt-[30px] h-fit">
       <Header />
       <SignUpForm />
       <Testimonials />
